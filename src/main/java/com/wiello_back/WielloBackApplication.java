ce5780ee34916c013efe4fecf9b27b2ac3bc1253
João Vitor Winter <joaovwinter3108@gmail.com>
2025-06-01 13:41:05 -0300

    Creating project | 01/06 13:41

---
+++ src/main/java/com/wiello_back/WielloBackApplication.java
@@ -0,0 +1,13 @@
+package com.wiello_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WielloBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WielloBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiello_back/WielloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiello_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WielloBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
